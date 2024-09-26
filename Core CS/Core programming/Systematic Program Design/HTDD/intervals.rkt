3afc17fa0895b42308edd078d618f4c026992a0c
David Bakare <bakaredavid007@gmail.com>
2024-09-26 10:40:37 +0100

    Update intervals.rkt

--- Core CS/Core programming/Systematic Program Design/HTDD/intervals.rkt
+++ Core CS/Core programming/Systematic Program Design/HTDD/intervals.rkt
@@ -2,9 +2,9 @@
 ;; interp. seat number in a row, 1 and 10 are aisle sits
 
 (define SN1 1) ; aisle
-(define SN1 5) ; middle
-(define SN1 10) ; aisle
+(define SN2 5) ; middle
+(define SN3 10) ; aisle
 
-(define (fn-for-seat-num sn) (... sn))
+(define (fn-for-seat-num sn) (... sn)) ;stub
 ;; Template Rule Used
 ;; - atomic non-distinct: Natural[1,10]
