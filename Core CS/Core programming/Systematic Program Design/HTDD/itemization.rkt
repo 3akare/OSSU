44a0323ad16ea1c8eceb21827f0b102533dbf52f
David Bakare <bakaredavid007@gmail.com>
2024-09-26 11:46:55 +0100

    Update itemization.rkt

--- Core CS/Core programming/Systematic Program Design/HTDD/itemization.rkt
+++ Core CS/Core programming/Systematic Program Design/HTDD/itemization.rkt
@@ -11,4 +11,16 @@
 (define CD1 false) ; has not started
 (define CD2 10) ; just started
 (define CD3 3) ; almost over
-(define CD4 "Complete") ; over
+(define CD4 "complete") ; countdown is over
+
+#;
+(define (fn-for-countdown cd) 
+        (cond [(false? cd) (...)]
+              [(and (number? cd)(<= 1 cd)(< cd 10)) (... cd)]
+              [else (...)]))
+
+;; Template rules used:
+;; - one of: 3 cases
+;; - atomic distinct: false
+;; - atomic non-distinct: Natural[1,10]
+;; - atomic distinct: "complete"
