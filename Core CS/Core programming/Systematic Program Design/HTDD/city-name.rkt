7c7824f038db61fb41f1e047a64d93ae0561fa05
David Bakare <bakaredavid007@gmail.com>
2024-09-26 10:19:56 +0100

    Update city-name.rkt

--- Core CS/Core programming/Systematic Program Design/HTDD/city-name.rkt
+++ Core CS/Core programming/Systematic Program Design/HTDD/city-name.rkt
@@ -1,4 +1,4 @@
-;; City Name is String
+;; CityName is String
 ;; interp. the name of a city
 (define CN1 "Lagos")
 (define CN1 "Abuja")
@@ -8,3 +8,20 @@
 
 ;; Template rules used:
 ;; - atomic non-distinct: string
+
+
+;; Function Definition
+;; CityName -> Boolean
+;; Produce true if the given city is Abuja
+
+;; (define (city? city) false) ; stub
+
+;; Tests
+(check-expect (city? "Abuja") true)
+(check-expect (city? "Lagos") false)
+
+;; Template
+(define (city? city) (... city))
+
+;; Final Function
+(define (city? city) (string=? "Abuja" city))
