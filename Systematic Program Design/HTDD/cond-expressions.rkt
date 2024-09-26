2648a8ef05cbcc970162cd56c61dbcc4bb10cfef
David Bakare <bakaredavid007@gmail.com>
2024-09-26 08:42:14 +0100

    Update cond-expressions.rkt

--- Systematic Program Design/HTDD/cond-expressions.rkt
+++ Systematic Program Design/HTDD/cond-expressions.rkt
@@ -1,4 +1,4 @@
-(require 2htpd/image)
+(require 2htdp/image)
 ;; Image -> String
 ;; Produces the aspect ratio of a given image
 
@@ -6,7 +6,7 @@
 (define img2 (rectangle 10 20 "solid" "red"))
 (define img3 (rectangle 10 10 "solid" "red"))
 
-(define (aspect-ratio img) "") ; stub
+;; (define (aspect-ratio img) "") ; stub
 
 ;; Tests
 (check-expect (aspect-ratio img1) "wide")
@@ -14,16 +14,20 @@
 (check-expect (aspect-ratio img3) "square")
 
 ;; Template
-(define (aspect-ratio img) (... img))
+;(define (aspect-ratio img) (... img))
 
 ;; Final Function
 
-(define (aspect-ratio img) (
-  if (> image-width (img) image-heigth (img))(
-    "wide"
-    if (= image-width (img) image-height (img))(
-      "square"
-      "tall"
-    )
-  )
+#;
+(define (aspect-ratio img)(
+       if (> (image-width img) (image-height img))
+          "wide"
+          (if (= (image-width img) (image-height img))
+              "square"
+              "tall")))
+
+(define (aspect-ratio img)(
+         cond [(>(image-width img) (image-height img)) "wide"]
+              [(= (image-width img) (image-height img)) "square"]
+              [else "tall"]
 ))
