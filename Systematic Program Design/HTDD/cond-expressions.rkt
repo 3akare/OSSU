20c018e6675c28274ffe25bfe5c90c90cc6874f6
David Bakare <bakaredavid007@gmail.com>
2024-09-26 08:27:27 +0100

    Create cond-expressions.rkt

---
+++ Systematic Program Design/HTDD/cond-expressions.rkt
@@ -0,0 +1,29 @@
+(require 2htpd/image)
+;; Image -> String
+;; Produces the aspect ratio of a given image
+
+(define img1 (rectangle 20 10 "solid" "red"))
+(define img2 (rectangle 10 20 "solid" "red"))
+(define img3 (rectangle 10 10 "solid" "red"))
+
+(define (aspect-ratio img) "") ; stub
+
+;; Tests
+(check-expect (aspect-ratio img1) "wide")
+(check-expect (aspect-ratio img2) "tall")
+(check-expect (aspect-ratio img3) "square")
+
+;; Template
+(define (aspect-ratio img) (... img))
+
+;; Final Function
+
+(define (aspect-ratio img) (
+  if (> image-width (img) image-heigth (img))(
+    "wide"
+    if (= image-width (img) image-height (img))(
+      "square"
+      "tall"
+    )
+  )
+))
