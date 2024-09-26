7ce0e7e6d69c6940b3253b18c8cc7deba80074dc
David Bakare <bakaredavid007@gmail.com>
2024-09-26 09:46:56 +0100

    Create city-name.rkt

---
+++ Core CS/Core programming/Systematic Program Design/HTDD/city-name.rkt
@@ -0,0 +1,10 @@
+;; City Name is String
+;; interp. the name of a city
+(define CN1 "Lagos")
+(define CN1 "Abuja")
+
+#;
+(define (fon-for-city cn) (... cn))
+
+;; Template rules used:
+;; - atomic non-distinct: string
